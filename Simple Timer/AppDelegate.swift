156880948b7bb921fdcad1c153cda4adb131c523
Eric Allen <decoy@internetalche.me>
2014-09-29 21:52:21 -0400

    Initial Commit

---
+++ Simple Timer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Simple Timer
+//
+//  Created by Eric Allen on 9/29/14.
+//  Copyright (c) 2014 InternetAlche.Me. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
